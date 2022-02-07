07268fa0a6aaba86de384390e72e871e476d1ea3
tvdhout <thijsvandenhout@live.nl>
2022-02-07 15:37:57 +0100

    two locations

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -1,10 +1,12 @@
 import os
-from flask import Flask
-
-from app.website import website_blueprint
+from flask import Flask, render_template
 
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="static", template_folder="templates")
 app.config['SERVER_NAME'] = os.getenv('FQDN')
 
-app.register_blueprint(website_blueprint, subdomain='www')
+
+@app.route('/if')
+@app.route('/')
+def index():
+    return render_template("homepage.html")
